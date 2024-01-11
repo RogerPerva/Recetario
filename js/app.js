b73812a98522689303aeba68317a59569c22b490
Rogerpervaz <ulisescodi@gmail.com>
2024-01-10 21:29:40 -0600

    Obteniendo datos de la url

--- js/app.js
+++ js/app.js
@@ -1,18 +1,42 @@
-// No es necesario de una API KEY 
-function iniciarApp(){
+// No es necesario de una API KEY
 
-    obtenerCategorias();
+function iniciarApp() {
+  const selectCategorias = document.querySelector("#categorias");
+  selectCategorias.addEventListener("change", seleccionarCategoria);
 
-    function obtenerCategorias(){
-        const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
-        fetch(url)   // => Llamado hacia una url
-            .then(respuesta => {  ///Entonces ¿que? 
-                return respuesta.json() //obtenemos respuesta de tipo json
-            })
-            .then(resultado =>{
-                console.log(resultado) // entonces... imprimimos los resultados en consola
-            })
-    }
+  obtenerCategorias();
+
+  function obtenerCategorias() {
+    const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
+    fetch(url) // => Llamado hacia una url
+      .then((respuesta) => {
+        ///Entonces ¿que?
+        return respuesta.json(); //obtenemos respuesta de tipo json
+      })
+      .then((resultado) => {
+        console.log(resultado.categories); // entonces... imprimimos los resultados en consola
+        mostrarCategorias(resultado.categories);
+      });
+  }
+
+  function mostrarCategorias(categorias = []) {
+    categorias.forEach((categoria) => {
+      const { strCategory } = categoria;
+      const option = document.createElement("OPTION");
+      option.value = strCategory;
+      option.textContent = strCategory;
+      selectCategorias.appendChild(option);
+    });
+  }
+
+  function seleccionarCategoria(e) {
+    const categoria = e.target.value;
+    const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+            .then(resultado=>{console.log(resultado)});
+  }
 }
 
-document.addEventListener('DOMContentLoaded', iniciarApp);
+document.addEventListener("DOMContentLoaded", iniciarApp);
